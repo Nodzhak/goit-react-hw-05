2d49f964397acf495645ad9dba525c6120ec5afb
Nodzhak <kostiantyn.nodzhak@gmail.com>
2024-03-15 10:18:50 +0100

    3
    3

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,17 +1,10 @@
 import { Link } from "react-router-dom";
-import css from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div className={css.container}>
-      <p className={css.info}>Oooppsss...</p>
-      <div className={css.text_info}>
-        The page are you looking for may have been moved, deleted or possibly
-        never existed
-      </div>
-      <Link to={`/`} className={css.link_home}>
-        Go HomePage 
-      </Link>
+    <div>
+      <h1>Ops, page is not found. Try again! Or not.</h1>
+      <Link to='/'>Go Home</Link>
     </div>
   );
 };
