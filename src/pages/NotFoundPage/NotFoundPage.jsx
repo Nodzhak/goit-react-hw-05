167b6a8a8fb0b5539bcaf73a39f71567ffde09e7
Nodzhak <kostiantyn.nodzhak@gmail.com>
2024-03-15 20:31:08 +0100

    12
    12

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.css'
 
 const NotFoundPage = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h2>Oops... nothing found...</h2>
       <Link to="/">Back to home</Link>
-    </>
+    </div>
   );
 };
 export default NotFoundPage;
